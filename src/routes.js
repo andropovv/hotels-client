51723a00fe4190a286567aba78e1bc0908152b72
andropovv <5andry8831@gmail.com>
2023-02-08 11:56:06 +0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,24 @@
+// const routes = useRoutes([
+//   {
+//     path: "/",
+//     element:
+//   },
+//   {
+//     path: "users",
+//     element: ,
+//     children: [
+//       { index: true, element: },
+//       {
+//         path: ":userId",
+//         element: <Outlet />,
+//         children: [
+//           { path: "profile", element: },
+//           { path: "edit", element: },
+//           { index: true, element: <Navigate to="./profile" /> },
+//           { path: "*", element: <Navigate to="../profile" /> },
+//         ],
+//       },
+//     ],
+//   },
+//   { path: "*", element: <Navigate to="/" /> },
+// ]);
